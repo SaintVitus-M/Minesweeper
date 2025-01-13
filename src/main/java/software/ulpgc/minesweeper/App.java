c04ae03bf441060faacc73b04cd957e2bfb35411
Vit <vit@email.com>
2025-01-13 20:13:20 +0000

    Main App implementation.

--- src/main/java/software/ulpgc/minesweeper/App.java
+++ src/main/java/software/ulpgc/minesweeper/App.java
@@ -1,7 +1,11 @@
 package software.ulpgc.minesweeper;
 
+import software.ulpgc.minesweeper.swing.SwingMainFrame;
+
+import java.awt.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main() {
+        EventQueue.invokeLater(() -> new SwingMainFrame().setVisible(true));
     }
 }
