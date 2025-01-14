a0be3c8d79995edd4a53489c6168f869c226760f
Vit <vit@email.com>
2025-01-14 23:22:56 +0000

    Add a game utility class to load all the images necessary for the game.

--- src/main/java/software/ulpgc/minesweeper/architecture/io/ImageLoader.java
+++ src/main/java/software/ulpgc/minesweeper/architecture/io/ImageLoader.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 
 public class ImageLoader {
-    public static final String PATH = "src/main/resources/";
+    public static final String PATH = "src/main/resources/game/";
 
     public static Image getImage(int i) {
         return new ImageIcon(PATH + i + ".png").getImage();
