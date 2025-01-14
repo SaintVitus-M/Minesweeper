5079acf1a6b7b86918aeb7659038cbef36ff468a
Vit <vit@email.com>
2025-01-14 23:24:37 +0000

    Add the game main frame and its top panel dialog.

--- src/main/java/software/ulpgc/minesweeper/swing/SwingMainFrame.java
+++ src/main/java/software/ulpgc/minesweeper/swing/SwingMainFrame.java
@@ -1,23 +1,44 @@
 package software.ulpgc.minesweeper.swing;
+import software.ulpgc.minesweeper.architecture.control.Command;
+import software.ulpgc.minesweeper.architecture.view.BoardDisplay;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Map;
 
 public class SwingMainFrame extends JFrame {
-    private final JLabel gameStatus = new JLabel("");
+    private final Map<String, Command> commands;
 
-    public SwingMainFrame() throws HeadlessException {
+    public SwingMainFrame(BoardDisplay display, SwingSettingsFrame settingsFrame, JLabel status, JButton faceButton, Map<String, Command> commands) throws HeadlessException {
+        this.commands = commands;
         setTitle("Minesweeper Game");
-        setLocationRelativeTo(null);
+        setLocationRelativeTo(settingsFrame);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-
-        add(new SwingGameBoard(16, 16, 40, gameStatus));
-        add(createStatusPanel(), BorderLayout.SOUTH);
+        setJMenuBar(createMenuBar());
+        add((Component) display, BorderLayout.CENTER);
+        add(new SwingTopPanel(faceButton), BorderLayout.NORTH);
+        add(createStatusPanel(status), BorderLayout.SOUTH);
         setResizable(false);
         pack();
     }
 
-    private Component createStatusPanel() {
+    private JMenuBar createMenuBar() {
+        JMenuBar menuBar = new JMenuBar();
+        JMenu gameMenu = new JMenu("Game");
+        JMenuItem settingsItem = new JMenuItem("Settings");
+        settingsItem.addActionListener(_ -> {
+            commands.get("settings").execute();
+            this.dispose();
+        });
+        JMenuItem exitItem = new JMenuItem("Exit");
+        exitItem.addActionListener(_ -> System.exit(0));
+        gameMenu.add(settingsItem);
+        gameMenu.add(exitItem);
+        menuBar.add(gameMenu);
+        return menuBar;
+    }
+
+    private Component createStatusPanel(JLabel gameStatus) {
         JPanel statusPanel = new JPanel();
         statusPanel.add(gameStatus, JLabel.CENTER);
         return statusPanel;
---
+++ src/main/java/software/ulpgc/minesweeper/swing/SwingTopPanel.java
@@ -0,0 +1,9 @@
+package software.ulpgc.minesweeper.swing;
+
+import javax.swing.*;
+
+public class SwingTopPanel extends JPanel {
+    public SwingTopPanel(JButton faceButton) {
+        add(faceButton, JButton.CENTER);
+    }
+}
